5808968deb0b3488ef9770d8bf2dce9ae58f08d6
skoochagian <107503078+skoochagian@users.noreply.github.com>
2022-07-11 13:44:38 -0400

    Setup for hw 14

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/skoochagian/HW14',
+      author='Sara Koochagian',
+      author_email='sk2hh@virginia.edu',
+      license='MIT',
+      packages=['bookdemo'])
